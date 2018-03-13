3d2f80a7dd4d991db40f424b5dad54146a6d6e13
PartyMix <gsmedleyfx@gmail.com>
2018-03-12 23:27:09 -0300

    setting up connection menu wip

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
 const path = require('path')
 const url = require('url')
 
@@ -13,6 +14,40 @@ function createWindow()
         protocol: 'file:',
         slashes: true
       }));
+
+      const connectionMenuTemplate = [
+          {
+              label:"Connect",
+              submenu:[
+                    {
+                        label:"Host",
+                        click (item, focusedWindow) {
+                            console.log(focusedWindow);
+                            
+                            //focusedWindow.hostServer();
+                        }
+                    },
+                    {
+                        label:"Connect",
+                        click (item, focusedWindow) {
+                            focusedWindow.activateSynchronization();
+                        }
+
+                    }
+              ]
+            },
+            {
+                label: 'View',
+                submenu: [
+                    {role: 'reload'},
+                    {role: 'forcereload'},
+                    {role: 'toggledevtools'}
+                ]
+            }
+      ];
+
+      const menu = Menu.buildFromTemplate(connectionMenuTemplate);
+      Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', createWindow);
--- sync.js
+++ sync.js
@@ -1,11 +1,29 @@
-var socket = new WebSocket("ws://echo.websocket.org");
-
-socket.onopen = function()
+function host()
 {
-    socket.send("dogs");
+    var WebSocketServer = require("websocketserver");
+    var server = new WebSocketServer("all", 9000);
+    activateSynchronization();
 }
 
-socket.onmessage = function(message)
+function activateSynchronization()
 {
-    console.log(message);
+    var socket = new WebSocket("ws://localhost:9000");
+
+    var yourInputField = document.getElementById("yours");
+    var theirInputField = document.getElementById("theirs");
+
+    socket.onopen = function()
+    {
+        
+    }
+
+    yourInputField.onkeypress = function()
+    {
+        socket.send(yourInputField.value);
+    }
+
+    socket.onmessage = function(message)
+    {
+        theirInputField.value = message.data;
+    }
 }
